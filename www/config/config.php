e294cc52db71a92001b4833d3fd3da2c93ab894e
Airi Iwamoto <s2tkaaa@gmail.com>
2021-11-10 03:14:09 +0900

    commit

--- www/config/config.php
+++ www/config/config.php
@@ -1,10 +1,19 @@
 <?php
 // ini_set('display_errors',1);
 
-define('DSN','mysql:host=db;
-dbname=ec_shop_test');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','27822750');
+require dirname(__FILE__).'/../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/../..');
+$dotenv->load();
+
+define('DSN','mysql:host='.$_ENV['HOST'].';
+dbname='.$_ENV['MYSQL_DATABASE']);
+define('DB_USERNAME',$_ENV['DB_USERNAME']);
+define('DB_PASSWORD',$_ENV['MYSQL_ROOT_PASSWORD']);
+
+// define('DSN','mysql:host=db;
+// dbname=ec_shop_test');
+// define('DB_USERNAME','root');
+// define('DB_PASSWORD','27822750');
 
 define('SITE_URL','http://'.$_SERVER['HTTP_HOST']);
 
